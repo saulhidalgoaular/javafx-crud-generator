be84d937845a097df2f889d68eceb1e1e429ea9f
Saul Hidalgo <saulhidalgoaular@gmail.com>
2023-01-22 12:35:16 +0300

    Sample Custom Button

--- src/test/java/CrudGeneratorTest.java
+++ src/test/java/CrudGeneratorTest.java
@@ -1,10 +1,13 @@
 import com.saulpos.javafxcrudgenerator.CrudGenerator;
 import com.saulpos.javafxcrudgenerator.CrudGeneratorParameter;
+import com.saulpos.javafxcrudgenerator.NodeConstructor;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -17,6 +20,21 @@ public class CrudGeneratorTest extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         CrudGeneratorParameter crudGeneratorParameter = new CrudGeneratorParameter();
+        NodeConstructor customButtonConstructor = new NodeConstructor() {
+            @Override
+            public Node generateNode(Object... name) {
+                Button customButton = new Button("Custom Button");
+                customButton.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        System.out.println("Custom Button");
+                    }
+                });
+                return customButton;
+            }
+        };
+
+        crudGeneratorParameter.getExtraButtonsConstructor().add(customButtonConstructor);
         CrudGenerator crudGenerator = new CrudGenerator(crudGeneratorParameter);
 
         stage.setTitle("Hello World!");
