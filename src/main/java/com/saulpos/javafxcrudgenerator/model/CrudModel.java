1f9e95602dff7c5ac19dfcee3c7c2b3827d47854
Saul Hidalgo <saulhidalgoaular@gmail.com>
2023-02-26 08:40:59 +0300

    Data Provider Changes

--- src/main/java/com/saulpos/javafxcrudgenerator/model/CrudModel.java
+++ src/main/java/com/saulpos/javafxcrudgenerator/model/CrudModel.java
@@ -20,13 +20,10 @@ import com.saulpos.javafxcrudgenerator.CrudGeneratorParameter;
 import com.saulpos.javafxcrudgenerator.model.dao.AbstractBean;
 import javafx.beans.property.*;
 import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class CrudModel<S extends AbstractBean> {
