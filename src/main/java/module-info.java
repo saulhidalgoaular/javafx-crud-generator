0023fc3d86a8a32956ab00e29a6807f6439ebf27
Saul Hidalgo <saulhidalgoaular@gmail.com>
2023-12-06 16:14:36 +0300

    Pom Fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module com.saulpos {
+module com.saulpos.crud {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
@@ -11,6 +11,11 @@ module com.saulpos {
 
     exports com.saulpos.javafxcrudgenerator;
     exports com.saulpos.javafxcrudgenerator.sample;
+    exports com.saulpos.javafxcrudgenerator.annotations;
+    exports com.saulpos.javafxcrudgenerator.model.dao;
+    exports com.saulpos.javafxcrudgenerator.model;
+    exports com.saulpos.javafxcrudgenerator.presenter;
+    exports com.saulpos.javafxcrudgenerator.view;
     opens com.saulpos.javafxcrudgenerator;
 
 }
